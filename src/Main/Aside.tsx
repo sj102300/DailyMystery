9f3c13abfc0d7aa42b722cb8318cccd5ec591cb4
류지승 <ryujiseung@lyujiseung-ui-MacBookPro.local>
2024-07-13 22:02:55 +0900

    0713 before chatgpt

--- src/Main/Aside.tsx
+++ src/Main/Aside.tsx
@@ -1,11 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Aside() {
     return (
         <div className="w-[5%] h-vdh pl-3 py-32 flex flex-col gap-5 z-10">
-            <div className="flex items-center justify-center flex-1 p-5 cursor-pointer bg-mainColor rounded-l-3xl">
+            <Link
+                to={"/main"}
+                className="flex items-center justify-center flex-1 p-5 cursor-pointer bg-mainColor rounded-l-3xl"
+            >
                 <div className="text-center font-[Pretendard-bold]">
                     심문하기
                 </div>
-            </div>
+            </Link>
             <div className="flex items-center justify-center flex-1 p-5 bg-white border-l-2 cursor-pointer border-y-2 rounded-l-3xl border-mainColor">
                 <div className="text-center font-[Pretendard-bold] text-mainColor">
                     사건현장
--- src/Main/SuspectNumber/SuspectNumber.tsx
+++ src/Main/SuspectNumber/SuspectNumber.tsx
@@ -1,10 +1,102 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
 import SuspectInfor from "./component/SuspectInfor";
+import { useStore } from "zustand";
+import storeSuspectInfor from "../../client/suspectInfor";
 
 export default function SuspectNumber() {
+    const { suspectNumber } = useParams();
+    const { suspectArray } = useStore(storeSuspectInfor);
+    // chatList scroll 될떄마다 스크롤 위치 바꾸기 위해서 ref를 갖고오고 변경되면 그떄에 따라 스크롤 위치 바꾸기
+    const chatListRef = useRef<HTMLDivElement>(null);
+
+    const [chatList, setChatList] = useState<string[]>([]);
+    const [inputValue, setInputValue] = useState<string>("");
+
+    const suspect: Suspect = suspectArray.find(
+        (val) => val.suspectNumber === Number(suspectNumber),
+    ) as Suspect;
+
+    const onsubmitConversation = (event: React.FormEvent) => {
+        event.preventDefault();
+        if (inputValue) {
+            setChatList((prevList) => [...prevList, inputValue]);
+            // gpt 요청
+            setInputValue("");
+        }
+    };
+
+    useEffect(() => {
+        if (chatListRef.current) {
+            chatListRef.current.scrollTop = chatListRef.current.scrollHeight;
+        }
+    }, [chatList]);
+
     return (
         <div className="z-10 flex items-center justify-around w-full py-32 h-dvh">
             <SuspectInfor />
-            <div className="w-3/5 h-full rounded-[20px] bg-white p-3 gap-3"></div>
+            <div className="w-3/5 h-full rounded-[20px] bg-white p-3 gap-3 flex flex-col">
+                <div className="border-[1px] border-black h-[15%] bg-[#f4f4f4] p-3 rounded-3xl flex justify-center items-center">
+                    <div className="text-black font-[Pretendard-Bold]">
+                        {suspect.suspectInfo}
+                    </div>
+                </div>
+                <div
+                    className="border-b-[1px] border-b-[#c1c1c1] w-full h-3/4 overflow-scroll text-mainColor"
+                    ref={chatListRef}
+                >
+                    {chatList.map((val, idx) =>
+                        idx % 2 === 0 ? (
+                            <div className="flex justify-end">
+                                <div className="bg-[#f4f4f4] border-[1px] border-[#e3e3e3] py-5 px-3 rounded-3xl max-w-[50%]">
+                                    {val}
+                                </div>
+                            </div>
+                        ) : (
+                            <div className="flex flex-col items-start gap-1">
+                                <img
+                                    src={suspect.suspectImageUrl}
+                                    alt="img"
+                                    className="rounded-full border-[1px] border-mainColor size-8"
+                                />
+                                <div className="bg-[#f4f4f4] border-[1px] border-[#e3e3e3] py-5 px-3 rounded-3xl max-w-[50%]">
+                                    {val}
+                                </div>
+                            </div>
+                        ),
+                    )}
+                </div>
+                <form
+                    className="w-full h-[10%] text-mainColor flex relative"
+                    onSubmit={onsubmitConversation}
+                >
+                    <input
+                        type="text"
+                        maxLength={120}
+                        placeholder="질문 기회는 총 5번이며 최대 120자 내로 질문할 수 있습니다."
+                        className="w-full h-full rounded-3xl bg-[#e3e3e3] pl-4 pr-16 outline-none"
+                        value={inputValue}
+                        onChange={(event) => setInputValue(event.target.value)}
+                    />
+                    <svg
+                        width="40"
+                        height="40"
+                        viewBox="0 0 40 40"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="absolute cursor-pointer right-3 top-[10%]"
+                        onClick={onsubmitConversation}
+                    >
+                        <path
+                            d="M26.6666 20L19.9999 13.3333M19.9999 13.3333L13.3333 20M19.9999 13.3333V26.6666M36.6666 20C36.6666 29.2047 29.2047 36.6666 19.9999 36.6666C10.7952 36.6666 3.33325 29.2047 3.33325 20C3.33325 10.7952 10.7952 3.33331 19.9999 3.33331C29.2047 3.33331 36.6666 10.7952 36.6666 20Z"
+                            stroke="#1E1E1E"
+                            strokeWidth="4"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                </form>
+            </div>
         </div>
     );
 }
