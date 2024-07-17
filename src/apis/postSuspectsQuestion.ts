21c9b68e373a89309541ae618ca28cceb6d37490
류지승 <ryujiseung@lyujiseung-ui-MacBookPro.local>
2024-07-17 11:14:32 +0900

    0717

--- src/apis/postSuspectsQuestion.ts
+++ src/apis/postSuspectsQuestion.ts
@@ -1,5 +1,3 @@
-import axios from "axios";
-
 export async function postSuspectsQuestion(
     suspectNumber: number,
     inputValue: string,
@@ -7,16 +5,51 @@ export async function postSuspectsQuestion(
     const userId = localStorage.getItem("userId");
 
     try {
-        const response = await axios.post(
+        const response = await fetch(
             `${import.meta.env.VITE_REACT_APP_BACK_URL}/api/suspects/${suspectNumber}/question`,
             {
-                userId: userId,
-                question: inputValue,
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    // "Content-Type": "text/event-stream" 헤더를 원한다면 주석을 해제하세요
+                },
+                body: JSON.stringify({
+                    userId: userId,
+                    question: inputValue,
+                }),
             },
         );
 
-        console.log(response.data);
-        return response;
+        if (response.ok && response.body) {
+            const reader = response.body
+                ?.pipeThrough(new TextDecoderStream())
+                .getReader();
+
+            const done = false;
+
+            while (!done) {
+                const { value, done: readerDone } = await reader.read();
+                if (readerDone) break;
+
+                const lines = value.split("\n");
+                for (const line of lines) {
+                    const trimmedLine = line.trim();
+
+                    if (trimmedLine) {
+                        let jsonString = trimmedLine;
+                        if (trimmedLine.startsWith("data:")) {
+                            jsonString = trimmedLine.slice(5).trim();
+                        }
+                        let data;
+                        if (jsonString && jsonString !== "[DONE]")
+                            data = JSON.parse(jsonString);
+
+                        if (data.choices && data.choices[0].delta.content)
+                            return data.choices[0].delta.content;
+                    }
+                }
+            }
+        }
     } catch (error) {
         console.log(error);
     }
