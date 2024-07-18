a1b73de2ae6a5e8f7ee65ce0cfe9a2c8ab8647dd
seungJoo <sj10230506@gmail.com>
2024-07-19 02:43:27 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <BrowserRouter basename="/SWDigital/">
+    <BrowserRouter basename="/">
         <App />
     </BrowserRouter>,
 );
