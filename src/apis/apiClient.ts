557efaf204a6b5b36d5d203eab95d53c5988796f
seungJoo <sj10230506@gmail.com>
2024-07-19 02:59:50 +0900

    update

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const host = window.location.hostname === "localhost" 
   ? 'http://43.202.161.19:8080'
-  : "api";
+  : "https://43.202.161.19:8080";
 
 export const apiClient = axios.create({
   baseURL: host,
