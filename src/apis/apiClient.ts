3bf25a8252a01fa8bd3d62927a5f721efd0ec9c2
seungJoo <sj10230506@gmail.com>
2024-07-19 03:57:05 +0900

    update

--- src/apis/apiClient.ts
+++ src/apis/apiClient.ts
@@ -1,6 +1,10 @@
 
 import axios from 'axios';
 
+const host = window.location.hostname === "localhost" 
+  ? import.meta.env.VITE_REACT_APP_BACK_URL
+  : import.meta.env.VITE_REACT_APP_BACK_PROXY
+
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_REACT_APP_BACK_PROXY,
+  baseURL: host,
 });
