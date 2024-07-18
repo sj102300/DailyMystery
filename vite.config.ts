90133b78199a06b57918559babecdbb510a5d0e4
seungJoo <sj10230506@gmail.com>
2024-07-19 02:28:42 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/SWDigital/",
+    base: './', // 이 줄을 추가하여 기본 경로를 상대 경로로 설정
     plugins: [react()],
 });
